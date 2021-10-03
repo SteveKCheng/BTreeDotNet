a91e0e5d5e5568795fa774cc063c650bf3961ca9
Steve Cheng <steve.ckp@gmail.com>
2021-10-03 10:07:48 -0400

    Add unit test of deletion from B+Tree

--- Tests/BTreeTests.cs
+++ Tests/BTreeTests.cs
@@ -7,32 +7,74 @@ namespace BPlusTree
 {
     public class BTreeTests
     {
-        [Fact]
-        public void Test1()
+        private static int[] GenerateRandomSequence(Random random, int count, bool unique)
         {
-            var btree = new BTree<int, int>(8, Comparer<int>.Default);
+            var items = new int[count];
 
-            var random = new Random(37);
+            var seen = unique ? new HashSet<int>() : null;
 
-            var seen = new HashSet<int>();
-            var items = new int[300];
-            for (int i = 0; i < items.Length; ++i)
+            for (int i = 0; i < count; ++i)
             {
                 int number;
                 do
                 {
                     number = random.Next(0, 10000);
-                } while (!seen.Add(number));
+                } while (seen != null ? !seen.Add(number) : false);
 
                 items[i] = number;
             }
 
+            return items;
+        }
+
+        [Fact]
+        public void AddItems()
+        {
+            var btree = new BTree<int, int>(8, Comparer<int>.Default);
+            var random = new Random(37);
+
+            var items = GenerateRandomSequence(random, 300, true);
+
             foreach (var number in items)
                 btree.Add(number, number);
+
+            Assert.Equal(items.Length, btree.Count);
 
             var sortedItems = items.OrderBy(x => x).ToArray();
 
             var outputs = btree.ToArray();
+            Assert.All(outputs, item => Assert.Equal(item.Key, item.Value));
+            Assert.Equal(sortedItems, outputs.Select(item => item.Key));
+        }
+
+        [Fact]
+        public void AddAndDeleteItems()
+        {
+            var btree = new BTree<int, int>(8, Comparer<int>.Default);
+            var random = new Random(37);
+            var items = GenerateRandomSequence(random, 300, true);
+
+            var removed = new List<int>();
+
+            for (int i = 0; i < items.Length; ++i)
+            {
+                var number = items[i];
+                btree.Add(number, number);
+
+                if ((i % 5) == 4)
+                {
+                    var numberToRemove = items[i - 4];
+                    btree.Remove(numberToRemove);
+                    removed.Add(numberToRemove);
+                }
+            }
+
+            var sortedItems = items.Except(removed).OrderBy(x => x).ToArray();
+
+            Assert.Equal(sortedItems.Length, btree.Count);
+
+            var outputs = btree.ToArray();
+
             Assert.All(outputs, item => Assert.Equal(item.Key, item.Value));
             Assert.Equal(sortedItems, outputs.Select(item => item.Key));
         }
